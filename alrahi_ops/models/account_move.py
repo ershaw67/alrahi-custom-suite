ec8fe56122522a60b6361aa109affd92d4c15859
ershaw67 <ershad.km.67@gmail.com>
2026-04-26 15:58:34 +0400

    Update account_move.py

--- alrahi_ops/models/account_move.py
+++ alrahi_ops/models/account_move.py
@@ -3,17 +3,17 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    # 1. Text Field: Customer TRN (Compliance)
-    x_customer_trn = fields.Char(string="Customer TRN")
+    # 1. Text Field: Customer TRN
+    x_customer_trn = fields.Char(string="Customer TRN", help="Tax Registration Number")
 
-    # 2. Selection Panel: Delivery Method (Logistics)
+    # 2. Selection Panel: Delivery Method
     x_delivery_type = fields.Selection([
         ('pickup', 'Self Pickup'),
         ('van', 'Alrahi Delivery Van'),
         ('courier', 'Third-Party Courier')
     ], string="Delivery Method", default='van')
 
-    # 3. Date Field: Roasting Date (Traceability)
+    # 3. Date Field: Roasting Date
     x_roast_date = fields.Date(string="Roast Date")
 
     # 4. Integer Field: Batch Number
@@ -22,5 +22,5 @@ class AccountMove(models.Model):
     # 5. Boolean Field: Quality Certified
     x_quality_certified = fields.Boolean(string="Quality Certified", default=False)
 
-    # 6. Many2one Field: Assigned Driver (Relational Data)
+    # 6. Many2one Field: Assigned Driver (Relational)
     x_driver_id = fields.Many2one('res.users', string="Assigned Driver")
