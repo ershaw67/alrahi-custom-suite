7edea47488ada76299551a431ec0b0bebfeeadaa
ershaw67 <ershad.km.67@gmail.com>
2026-04-26 08:29:42 +0400

    Create __manifest__.py

---
+++ alrahi_ops/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Alrahi Operations Suite',
+    'version': '1.0',
+    'summary': 'Custom Inventory and Accounting for Alrahi Group',
+    'category': 'Operations',
+    'author': 'Ershad KM',
+    'depends': ['stock', 'account'], # This links to Inventory and Accounting
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
