462f1940ceb9c4453c1e1840c6f6ddc477c45208
ershaw67 <ershad.km.67@gmail.com>
2026-04-26 15:57:01 +0400

    Update __manifest__.py

--- alrahi_ops/__manifest__.py
+++ alrahi_ops/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    'name': 'Alrahi Operations Suite',
+    'name': 'Alrahi Group: Invoice Operations Suite',
     'version': '1.0',
-    'summary': 'Custom Inventory and Accounting for Alrahi Group',
-    'category': 'Operations',
-    'author': 'Ershad KM',
-    'depends': ['stock', 'account'], # This links to Inventory and Accounting
-    'data': [],
+    'summary': 'Custom 6-field layout for Roastery Logistics',
+    'author': 'Your Name',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
